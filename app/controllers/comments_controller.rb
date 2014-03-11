18c45b30a36968e622eac7d75e6ec9bfbe95cff5
Andrew Wittrock <awittrock@gmail.com>
2014-03-11 00:49:00 -0500

    working on comment editting functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,13 +7,23 @@ class CommentsController < ApplicationController
 
   def show
     @post = Post.where(post_id: params[:id]).first
+  end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
   end
 
   def create
-
     @post = (params[:post_id])
     @comment = Comment.create(user_id: session[:user_id], post_id: params[:post_id], text: params[:comment][:text])
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
     redirect_to post_path(@post)
   end
 end
